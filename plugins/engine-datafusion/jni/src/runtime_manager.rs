cf92ae940d0cb637339acdbabd6881273e7e9081
Ajay Raj Nelapudi <ajnelapu@amazon.com>
2026-04-23 22:13:45 +0530

    Set CPU and IO to 1.5x cores

--- plugins/engine-datafusion/jni/src/runtime_manager.rs
+++ plugins/engine-datafusion/jni/src/runtime_manager.rs
@@ -127,8 +127,8 @@ impl RuntimeManager {
         Self::with_config(RuntimeConfig::new()
             .with_cpu_threads(cpu_threads)
             .with_io_threads(cpu_threads)
-            .with_cpu_multiplier(1.0)
-            .with_io_multiplier(2.0)
+            .with_cpu_multiplier(1.5)
+            .with_io_multiplier(1.5)
         )
     }
 
